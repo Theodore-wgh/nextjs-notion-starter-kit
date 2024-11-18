465c77d09509fbd638617b3a2289b31b4a74fca6
Guihong Wang <143874815+Theodore-wgh@users.noreply.github.com>
2024-11-18 20:50:30 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '1424d29706348003b02be5161d044379',
+  rootNotionPageId: '1424d297063480daba22e60421ed9dd5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Math Phd to Learn Everything',
-  domain: 'https://solar-dolphin-de1.notion.site/Numerical-Method-1424d29706348003b02be5161d044379',
+  domain: 'https://solar-dolphin-de1.notion.site/Navigation-1424d297063480daba22e60421ed9dd5',
   author: 'wgh',
 
   // open graph metadata (optional)
